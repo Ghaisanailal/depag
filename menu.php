df775995692c16747b8d4d668eda9794bc9679b5
Ghaisanailal <ghaisa.nailal18@smk.belajar.id>
2024-10-04 09:42:03 +0700

    update apk

---
+++ menu.php
@@ -0,0 +1,18 @@
+ <!-- Brand -->
+ <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">
+          <i class="glyphicon glyphicon-asterisk"></i>
+          Data Kemenag </a>
+      </div>
+       <!-- Brand -->
+ <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">
+          <i class="glyphicon glyphicon-user"></i>
+          Data pegawai </a>
+      </div>
+
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">
+          <i class="glyphicon glyphicon-user"></i>
+          Berita </a>
+      </div>
